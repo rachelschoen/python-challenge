9143330fe71539057fa6e6d72fcbc81833c769c2
Rachel Schoen <rachelschoen7@gmail.com>
2024-03-20 21:48:22 -0400

    update main.py with some work

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,3 +9,56 @@ with open('./Resources/election_data.csv', 'r') as myFile:
     reader = csv.DictReader(myFile)
     electionData = list(reader)
 
+# Print header
+print("Election Results")
+print("-------------------------")
+
+# Print the total number of votes cast.
+numVotes = len(electionData)
+print(f"Total Votes: {numVotes}")
+print("-------------------------")
+
+# Sort the nested list by candidate.
+sortedElectionData = sorted(electionData, key=itemgetter('Candidate'))
+
+# Next, we'll calculate each required calculation step-by-step.
+
+# 1) Count the number of votes, calculate the percentage of election won, and then add to the message list.
+
+
+#     a) Prepare the variables for the iteration of all votes.
+name = sortedElectionData[0]['Candidate']
+numCandidates = 0
+voteCount = 0
+winnerName = ""
+winnerPercentage = 0
+message = []
+
+#     b) Enter for-loop, knowing the list of votes is sorted by candidate.)
+for i in range(0, numVotes):
+    row = sortedElectionData[i]
+
+    # If the name matches the candidate, add to the voteCount.
+    if row['Candidate'] == name:
+        voteCount += 1
+    else:
+        # Calculate percentage of votes out of total.
+        percentage = round(voteCount/numVotes*100, 3)
+        
+        # Check if it beats the largest percentage we have stored.
+        if percentage > winnerPercentage:
+            winnerName = name
+            winnerPercentage = percentage
+        
+        # Add our previous candidate's name, votes, and percentage to the message list.
+        print(f"{name}: {percentage}% ({voteCount})")
+        message.append(name + ": " + str(percentage) + "% " + "(" + str(voteCount) + ")")
+        
+        # Update the variables for our current candidate.
+        numCandidates += 1
+        name = row['Candidate']
+        voteCount = 1 # This is because we did not add a vote to the count yet.
+
+
+# 2) We've finished the for-loop, but we still need to print the last candidate's name, votes, and percentage.
+        
